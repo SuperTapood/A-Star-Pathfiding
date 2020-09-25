36036c76c24ab156bca10db2cfda1d16366dc67d
SuperTapood <yoav.o252@gmail.com>
2020-09-25 08:33:31 +0300

    algorithm will now count how long it took and loop forever

--- main.py
+++ main.py
@@ -22,6 +22,7 @@
 from cell import Cell
 import numpy as np
 import pygame
+from time import time
 
 
 def h(node):
@@ -37,49 +38,57 @@ def get_min(arr):
             mini = min_item.f_score
     return min_item
 
-grid = []
-cols, rows = 50, 50
-for i in range(cols):
-    grid.append([Cell(i, j) for j in range(rows)])
-grid = np.array(grid)
-width, height = 600, 600
-scr = pygame.display.set_mode((width, height))
-pygame.display.set_caption("A* Pathfinding")
-cell_width = (width - 50) / cols
-cell_height = (height - 50) / rows
-start = grid[0][0]
-start.wall = False
-end = grid[-1][-1]
-end.wall = False
-start.g_score = 0
-start.f_score = h(start)
-open_set = [start]
-path = []
 
 while True:
-    scr.fill((0, 0, 0))
+    s = time()
+    grid = []
+    cols, rows = 40, 40
+    for i in range(cols):
+        grid.append([Cell(i, j) for j in range(rows)])
+    grid = np.array(grid)
+    width, height = 600, 600
+    scr = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("A* Pathfinding")
+    cell_width = (width - 50) / cols
+    cell_height = (height - 50) / rows
+    start = grid[0][0]
+    start.wall = False
+    end = grid[-1][-1]
+    end.wall = False
+    start.g_score = 0
+    start.f_score = h(start)
+    open_set = [start]
+    path = []
+
+    while True:
+        scr.fill((0, 0, 0))
+        if open_set == []:
+            # no solution
+            break
+        current = get_min(open_set)
+        current.visited = True
+        if current == end:
+            break
+        open_set.remove(current)
+        for neighbor in current.get_pals(grid):
+            if current.g_score < neighbor.g_score and not neighbor.wall:
+                neighbor.parent = current
+                neighbor.g_score = current.g_score
+                neighbor.f_score = neighbor.g_score + h(neighbor)
+                if not neighbor in open_set:
+                    open_set.append(neighbor)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+        for cell in grid.flatten():
+            cell.draw(cell_width, cell_height, scr)
+        current.draw_green(cell_width, cell_height, scr)
+        pygame.display.update()
     if open_set == []:
-        # no solution
-        break
-    current = get_min(open_set)
-    current.visited = True
-    if current == end:
-        break
-    open_set.remove(current)
-    for neighbor in current.get_pals(grid):
-        if current.g_score < neighbor.g_score and not neighbor.wall:
-            neighbor.parent = current
-            neighbor.g_score = current.g_score
-            neighbor.f_score = neighbor.g_score + h(neighbor)
-            if not neighbor in open_set:
-                open_set.append(neighbor)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-    for cell in grid.flatten():
-        cell.draw(cell_width, cell_height, scr)
-    current.draw_green(cell_width, cell_height, scr)
-    pygame.display.update()
+        print("Failed in ", end="")
+    else:
+        print("Done in ", end="")
+    print(time() - s, end=" seconds\n")
 
 par = current
 while True:
